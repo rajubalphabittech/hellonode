8d127d431d7d041357594d6c7e2c59161dd6b89e
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-06 22:09:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("we are now edited\n");
+  response.end("Running in local\n");
 });
 
 // listen on localhost:8000
 server.listen(9000);
-console.log("we are  now listening to server at http://127.0.0.1:9000/");
+console.log("we now in local://127.0.0.1:9000/");
